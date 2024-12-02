40e91d063347355fe75734a49bd6d72931d008d6
JohnGWebDev <john1galiszewski@gmail.com>
2024-12-01 22:43:48 -0500

    updated private page mixin and landing page typos

--- awqa/core/middleware.py
+++ awqa/core/middleware.py
@@ -13,7 +13,6 @@ class HtmxResponseMiddleware(object):
     def process_template_response(self, request, response):
         """Middleware hook called after a view executes."""
         app_name = resolve(request.path).app_name
-        if (app_name in settings.HTMX_APPS or app_name == '') and not response.headers.get("HX-Retarget"):
+        if (app_name in settings.HTMX_APPS or app_name == ''):
             response.template_name = f"{response.template_name[0]}{'#page-partial' if request.htmx else ''}"
-        print(response.template_name)
         return response
